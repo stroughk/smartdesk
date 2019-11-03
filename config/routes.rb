0ce975315795c07f7a50ab460a35078efb5a2466
stroughk <katerynastrough@hotmail.com>
2019-11-03 14:36:03 -0600

    create a login page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   resources :tickets
 
-get 'signup', to: 'users#new'
-resources :users, except: [:new]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
