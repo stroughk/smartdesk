a6177f750b25209f0a74fc88b5938b9badc36281
stroughk <katerynastrough@hotmail.com>
2019-11-03 08:33:46 -0600

    add user migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_155510) do
+ActiveRecord::Schema.define(version: 2019_11_03_142926) do
 
   create_table "tickets", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_11_02_155510) do
     t.datetime "updated_at"
     t.integer "user_id"
     t.boolean "complete", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
