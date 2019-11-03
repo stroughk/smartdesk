cd9ffa8ae96eacf6d2081538e77a447f24dd7115
stroughk <katerynastrough@hotmail.com>
2019-11-03 09:31:59 -0600

    add bcrypt gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_144201) do
+ActiveRecord::Schema.define(version: 2019_11_03_152934) do
 
   create_table "tickets", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_11_03_144201) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
