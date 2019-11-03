751c48e7ff54fcba6fb3ab6a5d7dab00181a6d99
stroughk <katerynastrough@hotmail.com>
2019-11-03 14:46:59 -0600

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,21 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = "You are now logged in"
+            redirect_to user_path(user)
+        else
+            flash.now[:danger] = "You login was not successful. Please try again or sign up to use Smartdesk."
         render 'new'
-    end
+        end
+    end        
 
     def destroy
-
+        session[:user_id] = nil
+        flash[:success] = "You have successfully logged out"
+        redirect_to root_path
     end
 
 end
