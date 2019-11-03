7abaae30e0bcd15c83662cc2f276cdc7a54ac6f8
stroughk <katerynastrough@hotmail.com>
2019-11-03 14:10:32 -0600

    add users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
@@ -27,6 +32,9 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
 
     private
 
