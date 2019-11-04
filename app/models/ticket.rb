69175b2784a3a33e5b7a09f40f7e7ffb73deb249
stroughk <katerynastrough@hotmail.com>
2019-11-04 17:57:43 -0600

    debug errors with pry

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,5 +2,5 @@ class Ticket < ApplicationRecord
     belongs_to :user
     validates :title, presence: true, length: { minimum: 5, maximum: 25 }
     validates :description, presence: true, length: { minimum: 10, maximum: 100 }
-    validates :users_id, presence: true
+    validates :user_id, presence: true
 end
