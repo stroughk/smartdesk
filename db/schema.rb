32cab64a3f1bd764f9e3ddfb6ad342cb3f038c64
stroughk <katerynastrough@hotmail.com>
2019-11-06 17:46:54 -0600

    create category model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_002906) do
+ActiveRecord::Schema.define(version: 2019_11_06_233537) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "title"
