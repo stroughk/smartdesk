472437ac7dbab46385362c519c839cf93e655f79
stroughk <katerynastrough@hotmail.com>
2019-11-05 18:24:34 -0600

    fix the logout route in routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  get 'logout' => 'sessions#destroy'
   delete 'logout', to: 'sessions#destroy'
 end
