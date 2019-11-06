8c0a705a0f4468056089371cd0c8604d39c32bc2
stroughk <katerynastrough@hotmail.com>
2019-11-05 19:43:32 -0600

    modify agent role to be able to delete users and tickets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
     before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_agent, only: [:destroy]
 
     def index
         @users = User.all
@@ -37,6 +38,14 @@ class UsersController < ApplicationController
     def show 
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        flash[:danger] = "Client and all tickets have been deleted"
+        redirect_to users_path
+    end
+
+
     private
 
     def user_params
@@ -48,9 +57,16 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if current_user != @user
+        if current_user != @user and !current_user.agent?
             flash[:danger] = "You can only edit your own account"
             redirect_to root_path
         end
+    end
+
+    def require_agent
+        if logged_in? and !current_user.agent?
+            flash[:danger] = "Only Helpdesk Agents can do this"
+            redirect_to root_path
+        end            
     end
 end
