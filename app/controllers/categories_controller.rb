cb00fa6a3aacc3a7807a48d7a4f1390c73ddb55c
stroughk <katerynastrough@hotmail.com>
2019-11-06 19:40:51 -0600

    add feature that only agents can create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+    before_action :require_agent, except:[:index, :show]
+
     def index
         @categories = Category.all
     end
@@ -26,5 +28,13 @@ class CategoriesController < ApplicationController
     def category_params
         params.require(:category).permit(:name)
     end
+
+    def require_agent
+        if !logged_in? || (logged_in? and !current_user.agent?)
+            flash[:danger] = "Only Smartdesk agents are permitted to do this."
+            redirect_to categories_path
+        end
+    end
+
 
 end
